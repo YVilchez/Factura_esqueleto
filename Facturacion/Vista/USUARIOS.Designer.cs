0b5a6a1ecf2ad4fc883907ae5743f76788919947
yohanna <yohannavilchez02@gmail.com>
2023-03-11 19:49:04 +0000

    USUARIOS

--- Facturacion/Vista/USUARIOS.Designer.cs
+++ Facturacion/Vista/USUARIOS.Designer.cs
@@ -29,6 +29,7 @@ namespace Vista
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.tbcodigo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,11 +39,11 @@ namespace Vista
             this.tbcorreo = new System.Windows.Forms.TextBox();
             this.cvv = new System.Windows.Forms.Label();
             this.Rol = new System.Windows.Forms.Label();
-            this.cbrol = new System.Windows.Forms.ComboBox();
+            this.tbrol = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.chec = new System.Windows.Forms.CheckBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbfoto = new System.Windows.Forms.PictureBox();
             this.btfoto = new System.Windows.Forms.Button();
             this.btnuevo = new System.Windows.Forms.Button();
             this.btguardar = new System.Windows.Forms.Button();
@@ -50,8 +51,10 @@ namespace Vista
             this.btteliminar = new System.Windows.Forms.Button();
             this.btcancelar = new System.Windows.Forms.Button();
             this.datausuarios = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbfoto)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.datausuarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -65,7 +68,8 @@ namespace Vista
             // 
             // tbcodigo
             // 
-            this.tbcodigo.Location = new System.Drawing.Point(168, 39);
+            this.tbcodigo.Enabled = false;
+            this.tbcodigo.Location = new System.Drawing.Point(168, 42);
             this.tbcodigo.Name = "tbcodigo";
             this.tbcodigo.Size = new System.Drawing.Size(438, 30);
             this.tbcodigo.TabIndex = 1;
@@ -73,7 +77,7 @@ namespace Vista
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(75, 106);
+            this.label2.Location = new System.Drawing.Point(66, 112);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 25);
             this.label2.TabIndex = 2;
@@ -81,22 +85,25 @@ namespace Vista
             // 
             // tbnombre
             // 
-            this.tbnombre.Location = new System.Drawing.Point(168, 106);
+            this.tbnombre.Enabled = false;
+            this.tbnombre.Location = new System.Drawing.Point(168, 109);
             this.tbnombre.Name = "tbnombre";
             this.tbnombre.Size = new System.Drawing.Size(438, 30);
             this.tbnombre.TabIndex = 3;
             // 
             // tbcontrase
             // 
-            this.tbcontrase.Location = new System.Drawing.Point(168, 168);
+            this.tbcontrase.Enabled = false;
+            this.tbcontrase.Location = new System.Drawing.Point(168, 171);
             this.tbcontrase.Name = "tbcontrase";
+            this.tbcontrase.PasswordChar = '*';
             this.tbcontrase.Size = new System.Drawing.Size(438, 30);
             this.tbcontrase.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(48, 171);
+            this.label3.Location = new System.Drawing.Point(33, 171);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 25);
             this.label3.TabIndex = 4;
@@ -104,7 +111,8 @@ namespace Vista
             // 
             // tbcorreo
             // 
-            this.tbcorreo.Location = new System.Drawing.Point(168, 232);
+            this.tbcorreo.Enabled = false;
+            this.tbcorreo.Location = new System.Drawing.Point(168, 235);
             this.tbcorreo.Name = "tbcorreo";
             this.tbcorreo.Size = new System.Drawing.Size(438, 30);
             this.tbcorreo.TabIndex = 7;
@@ -121,22 +129,24 @@ namespace Vista
             // Rol
             // 
             this.Rol.AutoSize = true;
-            this.Rol.Location = new System.Drawing.Point(75, 296);
+            this.Rol.Location = new System.Drawing.Point(107, 299);
             this.Rol.Name = "Rol";
             this.Rol.Size = new System.Drawing.Size(40, 25);
             this.Rol.TabIndex = 8;
             this.Rol.Text = "Rol";
             // 
-            // cbrol
+            // tbrol
             // 
-            this.cbrol.FormattingEnabled = true;
-            this.cbrol.Items.AddRange(new object[] {
+            this.tbrol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tbrol.Enabled = false;
+            this.tbrol.FormattingEnabled = true;
+            this.tbrol.Items.AddRange(new object[] {
             "Administrador",
             "Usuario"});
-            this.cbrol.Location = new System.Drawing.Point(168, 296);
-            this.cbrol.Name = "cbrol";
-            this.cbrol.Size = new System.Drawing.Size(442, 33);
-            this.cbrol.TabIndex = 9;
+            this.tbrol.Location = new System.Drawing.Point(168, 299);
+            this.tbrol.Name = "tbrol";
+            this.tbrol.Size = new System.Drawing.Size(442, 33);
+            this.tbrol.TabIndex = 9;
             // 
             // label4
             // 
@@ -150,7 +160,8 @@ namespace Vista
             // chec
             // 
             this.chec.AutoSize = true;
-            this.chec.Location = new System.Drawing.Point(168, 379);
+            this.chec.Enabled = false;
+            this.chec.Location = new System.Drawing.Point(168, 382);
             this.chec.Name = "chec";
             this.chec.Size = new System.Drawing.Size(22, 21);
             this.chec.TabIndex = 11;
@@ -159,59 +170,69 @@ namespace Vista
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(497, 269);
+            this.label5.Enabled = false;
+            this.label5.Location = new System.Drawing.Point(497, 272);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 25);
             this.label5.TabIndex = 12;
             // 
-            // pictureBox1
+            // pbfoto
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Location = new System.Drawing.Point(631, 39);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(207, 290);
-            this.pictureBox1.TabIndex = 13;
-            this.pictureBox1.TabStop = false;
+            this.pbfoto.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pbfoto.Location = new System.Drawing.Point(637, 42);
+            this.pbfoto.Name = "pbfoto";
+            this.pbfoto.Size = new System.Drawing.Size(207, 290);
+            this.pbfoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbfoto.TabIndex = 13;
+            this.pbfoto.TabStop = false;
             // 
             // btfoto
             // 
-            this.btfoto.Location = new System.Drawing.Point(881, 285);
+            this.btfoto.Enabled = false;
+            this.btfoto.Location = new System.Drawing.Point(674, 359);
             this.btfoto.Name = "btfoto";
             this.btfoto.Size = new System.Drawing.Size(131, 44);
             this.btfoto.TabIndex = 14;
             this.btfoto.Text = "Foto";
             this.btfoto.UseVisualStyleBackColor = true;
+            this.btfoto.Click += new System.EventHandler(this.btfoto_Click);
             // 
             // btnuevo
             // 
-            this.btnuevo.Location = new System.Drawing.Point(80, 467);
+            this.btnuevo.Location = new System.Drawing.Point(80, 439);
             this.btnuevo.Name = "btnuevo";
             this.btnuevo.Size = new System.Drawing.Size(119, 53);
             this.btnuevo.TabIndex = 15;
             this.btnuevo.Text = "Nuevo";
             this.btnuevo.UseVisualStyleBackColor = true;
+            this.btnuevo.Click += new System.EventHandler(this.btnuevo_Click);
             // 
             // btguardar
             // 
-            this.btguardar.Location = new System.Drawing.Point(506, 467);
+            this.btguardar.Enabled = false;
+            this.btguardar.Location = new System.Drawing.Point(378, 439);
             this.btguardar.Name = "btguardar";
             this.btguardar.Size = new System.Drawing.Size(119, 53);
             this.btguardar.TabIndex = 16;
             this.btguardar.Text = "GUARDAR";
             this.btguardar.UseVisualStyleBackColor = true;
+            this.btguardar.Click += new System.EventHandler(this.btguardar_Click);
             // 
             // btmodificar
             // 
-            this.btmodificar.Location = new System.Drawing.Point(282, 467);
+            this.btmodificar.Enabled = false;
+            this.btmodificar.Location = new System.Drawing.Point(218, 439);
             this.btmodificar.Name = "btmodificar";
             this.btmodificar.Size = new System.Drawing.Size(138, 53);
             this.btmodificar.TabIndex = 17;
             this.btmodificar.Text = "MODIFICAR";
             this.btmodificar.UseVisualStyleBackColor = true;
+            this.btmodificar.Click += new System.EventHandler(this.btmodificar_Click);
             // 
             // btteliminar
             // 
-            this.btteliminar.Location = new System.Drawing.Point(700, 467);
+            this.btteliminar.Enabled = false;
+            this.btteliminar.Location = new System.Drawing.Point(512, 439);
             this.btteliminar.Name = "btteliminar";
             this.btteliminar.Size = new System.Drawing.Size(138, 53);
             this.btteliminar.TabIndex = 18;
@@ -220,12 +241,14 @@ namespace Vista
             // 
             // btcancelar
             // 
-            this.btcancelar.Location = new System.Drawing.Point(907, 467);
+            this.btcancelar.Enabled = false;
+            this.btcancelar.Location = new System.Drawing.Point(667, 439);
             this.btcancelar.Name = "btcancelar";
             this.btcancelar.Size = new System.Drawing.Size(138, 53);
             this.btcancelar.TabIndex = 19;
             this.btcancelar.Text = "CANCELAR";
             this.btcancelar.UseVisualStyleBackColor = true;
+            this.btcancelar.Click += new System.EventHandler(this.btcancelar_Click);
             // 
             // datausuarios
             // 
@@ -237,11 +260,15 @@ namespace Vista
             this.datausuarios.Size = new System.Drawing.Size(1068, 150);
             this.datausuarios.TabIndex = 20;
             // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
             // USUARIOS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 758);
+            this.ClientSize = new System.Drawing.Size(856, 758);
             this.Controls.Add(this.datausuarios);
             this.Controls.Add(this.btcancelar);
             this.Controls.Add(this.btteliminar);
@@ -249,11 +276,11 @@ namespace Vista
             this.Controls.Add(this.btguardar);
             this.Controls.Add(this.btnuevo);
             this.Controls.Add(this.btfoto);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbfoto);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.chec);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.cbrol);
+            this.Controls.Add(this.tbrol);
             this.Controls.Add(this.Rol);
             this.Controls.Add(this.tbcorreo);
             this.Controls.Add(this.cvv);
@@ -264,11 +291,12 @@ namespace Vista
             this.Controls.Add(this.tbcodigo);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "USUARIOS";
             this.Text = "USUARIOS";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbfoto)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.datausuarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -285,11 +313,11 @@ namespace Vista
         private System.Windows.Forms.TextBox tbcorreo;
         private System.Windows.Forms.Label cvv;
         private System.Windows.Forms.Label Rol;
-        private System.Windows.Forms.ComboBox cbrol;
+        private System.Windows.Forms.ComboBox tbrol;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.CheckBox chec;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbfoto;
         private System.Windows.Forms.Button btfoto;
         private System.Windows.Forms.Button btnuevo;
         private System.Windows.Forms.Button btguardar;
@@ -297,5 +325,6 @@ namespace Vista
         private System.Windows.Forms.Button btteliminar;
         private System.Windows.Forms.Button btcancelar;
         private System.Windows.Forms.DataGridView datausuarios;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- Facturacion/Vista/USUARIOS.cs
+++ Facturacion/Vista/USUARIOS.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Biblio_Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,150 @@ namespace Vista
         public USUARIOS()
         {
             InitializeComponent();
+        }
+
+        string tipoOpera;
+        private void habilitarControles()
+        {
+            tbcodigo.Enabled = true;
+            tbcontrase.Enabled = true;
+            tbcorreo.Enabled = true;
+            tbnombre.Enabled = true;
+            tbrol.Enabled = true;
+            chec.Enabled = true;
+            btfoto.Enabled = true;
+            btguardar.Enabled = true;
+            btcancelar.Enabled = true;
+        }
+
+        private void deshabilitarControles()
+        {
+            tbcodigo.Enabled = false;
+            tbcontrase.Enabled = false;
+            tbcorreo.Enabled = false;
+            tbnombre.Enabled = false;
+            tbrol.Enabled = false;
+            chec.Enabled = false;
+            btfoto.Enabled = false;
+            btguardar.Enabled = false;
+            btcancelar.Enabled = false;
+        }
+
+        private void LimpiarControles()
+        {
+            tbcodigo.Clear();
+            tbcontrase.Clear();
+            tbcorreo.Clear();
+            tbnombre.Clear();
+            tbrol.Text = "";
+            chec.Checked = false;
+            pbfoto.Image = null;
+
+
+        }
+
+
+        private void btnuevo_Click(object sender, EventArgs e)
+        {
+            tbcodigo.Focus();
+            habilitarControles();
+            tipoOpera = "Nuevo";
+
+        }
+
+
+        private void btcancelar_Click(object sender, EventArgs e)
+        {
+            deshabilitarControles();
+            LimpiarControles();
+        }
+
+        private void btguardar_Click(object sender, EventArgs e)
+        {
+            if (tipoOpera == "Nuevo")
+            {
+                if (string.IsNullOrEmpty(tbcodigo.Text))
+                {
+                    errorProvider1.SetError(tbcodigo, "Ingrese un codigo");
+                    tbcodigo.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                if (string.IsNullOrEmpty(tbnombre.Text))
+                {
+                    errorProvider1.SetError(tbnombre, "Ingrese un nombre");
+                    tbnombre.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+
+                if (string.IsNullOrEmpty(tbcontrase.Text))
+                {
+                    errorProvider1.SetError(tbcontrase, "ïngrese una contrase");
+                    tbcontrase.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                if (string.IsNullOrEmpty(tbrol.Text))
+                {
+                    errorProvider1.SetError(tbrol, "ïngrese un rol");
+                    tbrol.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+
+                //llamar a clase usuario datos
+                Usuario user = new Usuario();
+
+                user.codigoUsuario = tbcodigo.Text;
+                user.Nombre = tbnombre.Text;
+                user.Contra = tbcontrase.Text;
+                user.Correo = tbcorreo.Text;
+                user.Rol = tbrol.Text;
+                user.EstadoActivo = chec.Checked;
+
+                //Validar que contenga la fotografia 
+                if (pbfoto.Image != null)
+                {
+                    //Foto es ARREGLO DE BITS
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    //pasar pic box
+                    pbfoto.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    //pasar direcccion a use foto
+                    user.Foto = ms.GetBuffer();
+
+                }
+
+                //Insertar BDD
+            }
+
+
+            else if (tipoOpera == "Modificar")
+            {
+
+            }
+        }
+
+        private void btmodificar_Click(object sender, EventArgs e)
+        {
+            tipoOpera = "Modificar";
+        }
+
+        private void btfoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult resultado = dialog.ShowDialog();
+
+
+            if (resultado == DialogResult.OK)
+            {
+                pbfoto.Image = Image.FromFile(dialog.FileName);
+            }
+
         }
     }
 }
